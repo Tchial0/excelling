4a1ad87d8be613cdc7cf40bd2be348df8cb046aa
tchial0 <96700865+Tchial0@users.noreply.github.com>
2023-02-28 12:14:23 +0100

    Project Properties changes

--- Excelling/Properties/AssemblyInfo.cs
+++ Excelling/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Excelling")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Read and write excel file very simply.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Excelling")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Excelling")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
